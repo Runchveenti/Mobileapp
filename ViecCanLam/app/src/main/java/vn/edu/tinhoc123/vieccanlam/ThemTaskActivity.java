34929e5b85f4993ec7ad9d9ca0bf12ecf199eaa6
Runchveenti <nguyenngocanhthufpt77@gmail.com>
2023-11-08 16:16:34 +0700

    update

---
+++ ViecCanLam/app/src/main/java/vn/edu/tinhoc123/vieccanlam/ThemTaskActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.tinhoc123.vieccanlam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_them_task);
+    }
+}
